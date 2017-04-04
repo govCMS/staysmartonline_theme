0bd52fbbc5b60beb79ef9f0e3e014110a875c0f2
Nigel Cunningham <nigel@salsadigital.com.au>
2017-04-04 12:46:17 +1000

    [SSO-22-Revert] Revert changes - done instead by changing config.
    Signed-off-by: Nigel Cunningham <nigel@salsadigital.com.au>

--- stay_smart_2017/templates/node--alert.tpl.php
+++ stay_smart_2017/templates/node--alert.tpl.php
@@ -89,9 +89,7 @@
 
   <?php print render($title_prefix); ?>
   <?php if (!$page): ?>
-    <h<?php print $header_level + ' ' + $title_attributes; ?>>
-      <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
-    </h<?php print $header_level?>>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
